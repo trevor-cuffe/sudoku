dc4341a20d8e82355c8f1cb0a26c6f0c13dd5932
Trevor Cuffe <trevor.d.cuffe@gmail.com>
2020-09-17 00:27:07 +0000

    Add back-end with nodejs

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.get("/", (req, res) => {
+	res.render("sudoku.ejs");
+});
+
+
+app.listen(3000, () => {
+	console.log("serving on port 3000")
+});
